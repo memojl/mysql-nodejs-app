a2a9f10d3f25a0c14f0768796746346e10a1c6f1
Memo Jimenez <memojl08@gmail.com>
2021-06-01 20:16:28 -0500

    update 01.06.2021

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# mysql-nodejs-app
+# APP-NODEJS-MYSQL
+Desarrollo APP con NodeJS y Mysql
+
+# Pasos de Desarrollo
+1. Crear package.json [ npm init -y ]
+2. Instalar nodemon de forma Dev [ npm install nodemon -D ]
+3. Instalar [ npm i express colors morgan ]
+4. Instalar [ npm i express-handlebars express-session mysql express-mysql-session bcryptjs passport passport-local timeago.js connect-flash express-validator ]
---
+++ src/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const colors = require('colors');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+//settings
+app.set('port',process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  layoutsDir: path.join(app.get('views'), 'layouts'),
+  partialsDir: path.join(app.get('views'), 'partials'),
+  extname: '.hbs',
+  helpers: require('./lib/handlebars')
+}));
+app.set('view engine', '.hbs');
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//Global Vars
+app.use((req, res, next)=>{
+
+  next();
+});
+
+//routes
+app.use(require('./routes/RutasWeb'));
+//public
+
+//Server
+app.listen(app.get('port'), () => {
+  console.log(`Our app is running on port:`.blue + ` ${ app.get('port') }`.yellow);
+});
