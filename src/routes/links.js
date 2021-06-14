69bafc7366979dbfa35988c0ea779bce22d5d32f
Memo Jimenez <memojl08@gmail.com>
2021-06-14 09:38:02 -0500

    pre-update 14.06.2021

--- src/routes/links.js
+++ src/routes/links.js
@@ -3,12 +3,37 @@ const router = express.Router();
 
 const mysqlConnection = require('../database');
 
+//AGREGAR
 router.get('/add', (req, res)=>{
     res.render('links/add');
 });
 
-router.post('/add', (req, res)=>{
-    //res.render('links/add');
+router.post('/add', async (req, res)=>{
+    const {title,url,description} = req.body;
+    const newLink = {
+        title,
+        url,
+        description
+    }
+    //console.log(newLink);
+    await mysqlConnection.query('INSERT INTO links set ?', [newLink]);
+    //req.flash('success', 'Link Saved Successfully');
+    res.redirect('/links');
+});
+
+//LISTAR
+router.get('/', async (req, res)=>{
+    const links = await mysqlConnection.query('SELECT * FROM links');
+    res.render('links/list', { links });
+});
+
+
+//EDITAR
+router.get('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const links = await mysqlConnection.query('SELECT * FROM links WHERE id = ?', [id]);
+    console.log(links);
+    res.render('links/edit', {link: links[0]});
 });
 
 module.exports = router;
