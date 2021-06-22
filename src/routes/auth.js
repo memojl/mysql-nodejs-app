cb667c62fc318b1c79543ca5cecb15b42dd77ea9
Memo Jimenez <memojl08@gmail.com>
2021-06-22 18:00:20 -0500

    pre-update 22.06.2021

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+//REGISTRO-FORM
 router.get('/registro', (req, res)=>{
     res.render('auth/signup');
 });
 
+router.post('/registro', (req, res)=>{
+    console.log(req.body);
+    res.send('Recibido');
+});
+
+//LOGIN-FORM
 router.get('/login', (req, res)=>{
     res.render('auth/signin');
 });
